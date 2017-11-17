b3ce3a2620a1064c57bd62c3e732f38f93100def
Mike Matos <mikematos84@gmail.com>
2017-11-16 21:33:34 -0500

    Organization and JsonExample

---
+++ Assets/Scripts/Json/JsonExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JsonExample : MonoBehaviour
+{
+    string data;
+
+    // Use this for initialization
+    void Start()
+    {
+        TextAsset asset = (TextAsset)Resources.Load("example");
+        data = asset.text;
+
+        EmployeeData employee = JsonConvert.DeserializeObject<EmployeeData>(data);
+        Debug.Log(employee.dpn_profile.FirstName + "," + employee.dpn_profile.LastName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
